b0553f9d6ff874c714e477285366d46631e9822c
mitoand9 <mitoand9@yahoo.it>
2018-05-15 18:14:58 +0200

    dssw

---
+++ Lab3 final/Validation/Validation_Fast.m
@@ -0,0 +1,49 @@
+clear all
+
+ay_ADAMS = importdata('ay_fast.tab');
+ROLL_ADAMS = importdata('roll_fast.tab');
+yawrate_ADAMS = importdata('yawrate_fast.tab');
+
+load ay_Linear_Validated
+load yawrate_Linear_Validated
+load Time_Linear
+load ay_Brush_Validated
+load yawrate_Brush_Validated
+load Time_Brush
+
+load ay_VBOX_121
+load yawRate_VBOX_121
+load roll_VBOX_121
+load Time_VBOX_121
+
+figure(1)
+plot(ay_ADAMS(1:length(Time),1),-ay_ADAMS(1:length(Time),2)*9.81,'k')
+hold on
+plot(Time_Linear(1:length(ay_Linear_Validated)),ay_Linear_Validated,'b')
+hold on
+plot(Time_Brush(1:length(ay_Brush_Validated)),ay_Brush_Validated,'g')
+hold on
+plot(Time,ay_VBOX(:,1),'m')
+grid on
+xlabel('Time (s)')
+ylabel('a_y (m/s^2)')
+
+figure(2)
+plot(yawrate_ADAMS(1:length(Time),1),yawrate_ADAMS(1:length(Time),2)*pi/180,'k')
+hold on
+plot(Time_Linear(1:length(yawrate_Linear_Validated)),yawrate_Linear_Validated*pi/180,'b')
+hold on
+plot(Time_Brush(1:length(yawrate_Brush_Validated)),yawrate_Brush_Validated*pi/180,'g')
+hold on
+plot(Time,yawRate_VBOX(:,1),'m')
+grid on
+xlabel('Time (s)')
+ylabel('\Psi (rad/s)')
+
+figure(3)
+plot(ROLL_ADAMS(1:length(Time),1),ROLL_ADAMS(1:length(Time),2)*pi/180,'k')
+hold on
+plot(Time,roll_angle(1,:),'m')
+grid on
+xlabel('Time (s)')
+ylabel('\Phi (rad)')
