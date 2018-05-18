8da537c8068bf749dfa16e4c2bd57f152ad76742
mitoand9 <mitoand9@yahoo.it>
2018-05-18 10:03:11 +0200

    ssj

---
+++ Lab3 final/Validation/ARBTuning.m
@@ -0,0 +1,42 @@
+clear all
+
+ay_ADAMS = importdata('untuned-ay.tab');
+ROLL_ADAMS = importdata('untuned-roll.tab');
+yawrate_ADAMS = importdata('untuned-yaw.tab');
+
+ay_ADAMS_2 = importdata('tuned-ay.tab');
+ROLL_ADAMS_2 = importdata('tuned-roll.tab');
+yawrate_ADAMS_2 = importdata('tuned-yaw.tab');
+
+load ay_VBOX_121
+load yawRate_VBOX_121
+load roll_VBOX_121
+load Time_VBOX_121
+
+
+figure(1)
+plot(ay_ADAMS.data(1:length(Time),1),-ay_ADAMS.data(1:length(Time),2)*9.81,'k')
+hold on
+plot(Time,ay_VBOX(:,1),'m')
+grid on
+xlabel('Time (s)')
+ylabel('a_y (m/s^2)')
+legend('ADAMS','VBOX')
+
+figure(2)
+plot(yawrate_ADAMS.data(1:length(Time),1),yawrate_ADAMS.data(1:length(Time),2)*pi/180,'k')
+hold on
+plot(Time,yawRate_VBOX(:,1),'m')
+grid on
+xlabel('Time (s)')
+ylabel('\Psi (rad/s)')
+legend('ADAMS','VBOX')
+
+figure(3)
+plot(ROLL_ADAMS.data(1:length(Time),1),ROLL_ADAMS.data(1:length(Time),2)*pi/180,'k')
+hold on
+plot(Time,roll_angle(1,:),'m')
+grid on
+xlabel('Time (s)')
+ylabel('\Phi (rad)')
+legend('ADAMS','VBOX')
