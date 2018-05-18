fa360ce65448145a82000b3d43f7aab8b256ecf9
mitoand9 <mitoand9@yahoo.it>
2018-05-18 10:20:37 +0200

    kkn

--- Lab3 final/Validation/ARBTuning.m
+++ Lab3 final/Validation/ARBTuning.m
@@ -17,26 +17,32 @@ load Time_VBOX_121
 figure(1)
 plot(ay_ADAMS.data(1:length(Time),1),-ay_ADAMS.data(1:length(Time),2)*9.81,'k')
 hold on
+plot(ay_ADAMS_2.data(1:length(Time),1),-ay_ADAMS_2.data(1:length(Time),2)*9.81,'g')
+hold on
 plot(Time,ay_VBOX(:,1),'m')
 grid on
 xlabel('Time (s)')
 ylabel('a_y (m/s^2)')
-legend('ADAMS','VBOX')
+legend('ADAMS Untuned','ADAMS Tuned','VBOX')
 
 figure(2)
 plot(yawrate_ADAMS.data(1:length(Time),1),yawrate_ADAMS.data(1:length(Time),2)*pi/180,'k')
+hold on
+plot(yawrate_ADAMS_2.data(1:length(Time),1),yawrate_ADAMS_2.data(1:length(Time),2)*pi/180,'g')
 hold on
 plot(Time,yawRate_VBOX(:,1),'m')
 grid on
 xlabel('Time (s)')
 ylabel('\Psi (rad/s)')
-legend('ADAMS','VBOX')
+legend('ADAMS Untuned','ADAMS Tuned','VBOX')
 
 figure(3)
 plot(ROLL_ADAMS.data(1:length(Time),1),ROLL_ADAMS.data(1:length(Time),2)*pi/180,'k')
+hold on
+plot(ROLL_ADAMS_2.data(1:length(Time),1),ROLL_ADAMS_2.data(1:length(Time),2)*pi/180,'g')
 hold on
 plot(Time,roll_angle(1,:),'m')
 grid on
 xlabel('Time (s)')
 ylabel('\Phi (rad)')
-legend('ADAMS','VBOX')
+legend('ADAMS Untuned','ADAMS Tuned','VBOX')
