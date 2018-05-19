d5bff8e1abd867a922cb26677e47a1b7c3b00e8d
mitoand9 <mitoand9@yahoo.it>
2018-05-19 10:08:09 +0200

    ksks

--- Lab3 final/Validation/Validation_Fast_DLC.m
+++ Lab3 final/Validation/Validation_Fast_DLC.m
@@ -1,7 +1,7 @@
 clear all
 
-ay_ADAMS = importdata('DLC014_ay.tab');
-yawrate_ADAMS = importdata('DLC014_yaw.tab');
+ay_ADAMS = importdata('DLC011_ay.tab');
+yawrate_ADAMS = importdata('DLC011_yaw.tab');
 
 load ay_Linear_Validated_DLC011
 load yawrate_Linear_Validated_DLC011
@@ -15,7 +15,7 @@ load yawRate_VBOX_DLC011
 load Time_VBOX_DLC011
 
 figure(1)
-plot(ay_ADAMS.data(1:length(Time),1),-ay_ADAMS.data(1:length(Time),2)*9.81,'k')
+plot(ay_ADAMS.data(100:669,1)-0.99,-ay_ADAMS.data(100:669,2)*9.81,'k')
 hold on
 plot(Time_Linear_DLC011(1:length(ay_Linear_Validated_DLC011)),ay_Linear_Validated_DLC011,'b')
 hold on
@@ -28,7 +28,7 @@ ylabel('a_y (m/s^2)')
 legend('ADAMS','Linear','Brush','VBOX')
 
 figure(2)
-plot(yawrate_ADAMS.data(1:length(Time),1),yawrate_ADAMS.data(1:length(Time),2)*pi/180,'k')
+plot(yawrate_ADAMS.data(100:669,1)-0.99,yawrate_ADAMS.data(100:669,2)*pi/180,'k')
 hold on
 plot(Time_Linear_DLC011(1:length(yawrate_Linear_Validated_DLC011)),yawrate_Linear_Validated_DLC011*pi/180,'b')
 hold on
@@ -39,12 +39,3 @@ grid on
 xlabel('Time (s)')
 ylabel('\Psi (rad/s)')
 legend('ADAMS','Linear','Brush','VBOX')
-
-figure(3)
-plot(ROLL_ADAMS.data(1:length(Time),1),ROLL_ADAMS.data(1:length(Time),2)*pi/180,'k')
-hold on
-plot(Time,roll_angle(1,:),'m')
-grid on
-xlabel('Time (s)')
-ylabel('\Phi (rad)')
-legend('ADAMS','VBOX')
